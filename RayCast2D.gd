8d69d268f02b2345f34127b498aa19eb82e13948
jchristo <janou.christophe@gmail.com>
2020-10-04 14:11:40 +0200

    added a stationairy laser turret
    the laser turret turns on every 5 secs and then turns off for 5 seconds

---
+++ RayCast2D.gd
@@ -0,0 +1,48 @@
+extends RayCast2D
+
+
+var is_casting = false setget set_is_casting
+onready var timer = $Timer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	timer.start()
+	set_physics_process(false)
+	$Line2D.points[1] = Vector2.ZERO
+
+func _physics_process(delta):
+	var cast_point := cast_to
+	force_raycast_update()
+	
+	if is_colliding():
+		cast_point = to_local(get_collision_point())
+		
+	$Line2D.points[1] = cast_point
+
+func set_is_casting(cast : bool) -> void:
+	is_casting = cast
+	
+	if is_casting:
+		$powerUpLaser.play()
+		appear()
+	else:
+		$powerOffLaser.play()
+		disappear()
+	
+	set_physics_process(is_casting)
+	
+func appear():
+	$Tween.stop_all()
+	$Tween.interpolate_property($Line2D, "width", 0, 3.0, 0.2)
+	$Tween.start()
+	
+func disappear():
+	$Tween.stop_all()
+	$Tween.interpolate_property($Line2D, "width", 3.0, 0, 0.1)
+	$Tween.start()
+
+
+func _on_Timer_timeout():
+	self.is_casting = !self.is_casting
+	timer.start()
+	pass
