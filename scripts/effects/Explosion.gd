dbe00e350e00f113574d0745af6302e6abe884cc
Toc532 <theocremers123@msn.com>
2020-10-04 12:14:05 +0200

    explosion fix

--- scripts/effects/Explosion.gd
+++ scripts/effects/Explosion.gd
@@ -4,12 +4,12 @@ export(float) var damage_amount = 10
 
 func _ready():
 	$AnimatedSprite.playing = true
-	connect("area_entered", self, "_on_area_entered")
+	connect("body_entered", self, "_on_body_entered")
 
 func _on_AnimatedSprite_animation_finished():
 	call_deferred('free')
 
-func _on_area_entered(area):
-	if area.is_in_group("enemies"):
-		area.on_hit()
+func _on_body_entered(body):
+	if body.is_in_group("enemies"):
+		body.on_hit()
 	pass
