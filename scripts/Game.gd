c65b6f9dc73fcc6383b9d33752636f29495a9020
Toc532 <theocremers123@msn.com>
2020-10-05 13:10:15 +0200

    roomA5 start

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -3,7 +3,7 @@ extends Node2D
 var debug_gui_scene = preload("res://scenes/gui/DebugGUI.tscn")
 var general_gui_scene = preload("res://scenes/gui/GeneralGUI.tscn")
 
-export (String) var StartRoom = "RoomA1"
+export (String) var StartRoom = "RoomA5"
 
 const TIME_PENALTY: int = 10
 
